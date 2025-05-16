64fbe3399312f34c434cddef6179b8f024398b07
pdusara <pritesh.dusara@googlemail.com>
2025-05-16 11:39:30 +0100

    update print statement

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,5 +4,5 @@ import socket
 hostname = socket.gethostname()
 
 while True:
-    print(f"[POD: {hostname}] Hello, World! I deployed this via ArgoCD!!")
+    print(f"[POD: {hostname}] Hello, World! I deployed this via ArgoCD :)!!")
     time.sleep(5)
